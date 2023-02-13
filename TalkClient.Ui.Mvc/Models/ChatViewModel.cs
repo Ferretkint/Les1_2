bd97b02196ab770814a2a94c1f4dd9d388ffe153
Ferretkint <92437473+Ferretkint@users.noreply.github.com>
2023-02-13 15:35:23 +0100

    Initial commit

---
+++ TalkClient.Ui.Mvc/Models/ChatViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace TalkClient.Ui.Mvc.Models
+{
+    public class ChatViewModel
+    {
+        public IList<ChatChannel>? Channels { get; set; }
+        public IList<ChatMessage>? Messages { get;set; }
+    }
+}
